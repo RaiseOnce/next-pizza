c912218728f76eb9a49425951d933e29410d8498
Otto Rahn <elias.yunusoff@gmail.com>
2025-07-06 01:51:03 +0500

    checkbox filter is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 import { Container } from '@/components/shared/container'
+import { Filters } from '@/components/shared/filters'
+import { Title } from '@/components/shared/title'
 import { TopBar } from '@/components/shared/top-bar'
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
-        <header className="text-2xl uppercase font-black">Все пиццы</header>
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
         <TopBar />
+
+        <Container className="mt-10 pb-14">
+          <div className="flex gap-[60px]">
+            <div className="w-[250px]">
+              <Filters />
+            </div>
+
+            <div className="flex-1">
+              <div className="flex flex-col gap-16">Список товаров</div>
+            </div>
+          </div>
+        </Container>
       </Container>
     </>
   )
--- components/shared/aa-template.tsx
+++ components/shared/aa-template.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { cn } from '@/lib/utils' // обновлённый путь
-
+import { cn } from '@/lib/utils'
 interface Props {
   className?: string
 }
