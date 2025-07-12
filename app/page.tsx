3d41b762c1170e762b384ab54ffc9c232f98d8d8
Otto Rahn <elias.yunusoff@gmail.com>
2025-07-13 04:39:44 +0500

    product card is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@/components/shared/container'
 import { Filters } from '@/components/shared/filters'
+import ProductCard from '@/components/shared/product-card'
 import { Title } from '@/components/shared/title'
 import { TopBar } from '@/components/shared/top-bar'
 
@@ -17,7 +18,14 @@ export default function Home() {
             </div>
 
             <div className="flex-1">
-              <div className="flex flex-col gap-16">Список товаров</div>
+              <div className="flex flex-col gap-16">
+                <ProductCard
+                  id={0}
+                  name="Чизбургер-пицца"
+                  price={550}
+                  imageUrl="https://media.dodostatic.net/image/r:584x584/019591b642d87304a62d322945990861.avif"
+                />
+              </div>
             </div>
           </div>
         </Container>
