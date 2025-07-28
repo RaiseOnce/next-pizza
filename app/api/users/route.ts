60c243d80d2012da2b10cb536efc58dc4e27d37e
Otto Rahn <elias.yunusoff@gmail.com>
2025-07-28 11:36:18 +0500

    checkpoint for ingredients

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -10,6 +10,8 @@ export async function GET() {
 export async function POST(req: NextRequest) {
   const data = await req.json()
 
+  prisma
+
   const user = await prisma.user.create({
     data,
   })
