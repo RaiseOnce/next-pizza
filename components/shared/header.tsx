f488321e011433c0f95bcb1cd10ce461838faaef
Otto Rahn <elias.yunusoff@gmail.com>
2025-08-02 20:23:04 +0500

    search products is done

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -3,6 +3,8 @@ import { Container } from './container'
 import React from 'react'
 import { Button } from '../ui/button'
 import { ShoppingCart } from 'lucide-react'
+import Link from 'next/link'
+import { SearchInput } from './search-input'
 
 type Props = {
   className?: string
@@ -12,7 +14,13 @@ const Header: React.FC<Props> = ({ className }) => {
   return (
     <header className={cn('border border-b', className)}>
       <Container className="flex items-center justify-between py-8">
-        <div className="text-2xl uppercase font-black">Next Pizza</div>
+        <Link href="/">
+          <div className="text-2xl uppercase font-black">Next Pizza</div>
+        </Link>
+
+        <div className="mx-10 flex-1">
+          <SearchInput />
+        </div>
 
         <div className="flex items-center gap-3">
           <Button variant="outline">Войти</Button>
